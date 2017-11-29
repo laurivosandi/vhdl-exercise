d8523390858a8db2c8ad6f5c4d6b1540df4454c4
veonun <veonun@ttu.ee>
2017-11-29 23:19:43 +0200

    Fix logical NAND and NOR operations

--- alu.vhd
+++ alu.vhd
@@ -51,18 +51,18 @@ begin
             s => adder_result,
             co => adder_carry_out
         );
-        
+
     -- Logical NAND operation
-    nand_result(0) <= not m(0) and n(0);
-    nand_result(1) <= not m(1) and n(1);
-    nand_result(2) <= not m(2) and n(2);
-    nand_result(3) <= not m(3) and n(3);
+    nand_result(0) <= not (m(0) and not n(0));
+    nand_result(1) <= not (m(1) and not n(1));
+    nand_result(2) <= not (m(2) and not n(2));
+    nand_result(3) <= not (m(3) and not n(3));
 
     -- Logical NOR operation
-    nor_result(0) <= not m(0) or n(0);
-    nor_result(1) <= not m(1) or n(1);
-    nor_result(2) <= not m(2) or n(2);
-    nor_result(3) <= not m(3) or n(3);
+    nor_result(0) <= not (m(0) or n(0));
+    nor_result(1) <= not (m(1) or n(1));
+    nor_result(2) <= not (m(2) or n(2));
+    nor_result(3) <= not (m(3) or n(3));
 
     -- Select output based on which operation was requested
     d <=   nand_result when opcode ="10" else
